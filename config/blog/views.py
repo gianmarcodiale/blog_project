33f0693d4e10175f6d51c1c48f647b10b391a0ff
gianmarcodiale <gianmarcodialessandro@gmail.com>
2025-04-11 15:50:56 +0200

    added pagination, canonical urls, seo-friendly urls

--- config/blog/views.py
+++ config/blog/views.py
@@ -1,10 +1,18 @@
+from django.core.paginator import Paginator
 from django.shortcuts import get_object_or_404, render
 from .models import Post
 from django.http import Http404
 
 
 def post_list(request):
-    posts = Post.published.all()
+    post_list = Post.published.all()
+    # Pagination with 3 post per page
+    paginator = Paginator(post_list, 3)
+    # Retrieve the current page number
+    page_number = request.GET.get('page', 1)
+    # Get the current page posts
+    posts = paginator.page(page_number)
+
     return render(
         request,
         'blog/post/list.html',
@@ -12,15 +20,18 @@ def post_list(request):
     )
 
 
-def post_detail(request, id):
+def post_detail(request, year, month, day, post):
     #    try:
     #        post = Post.published.get(id=id)
     #    except Post.DoesNotExist:
     #        raise Http404("No Post found.")
     post = get_object_or_404(
         Post,
-        id=id,
-        status=Post.Status.PUBLISHED
+        status=Post.Status.PUBLISHED,
+        slug=post,
+        publish__year=year,
+        publish__month=month,
+        publish__day=day
     )
     return render(
         request,
